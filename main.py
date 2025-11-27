edae1c559f30d02d64525af0768aec098141c243
Tais Souza <tata.ssouz47@gmail.com>
2025-11-26 21:44:09 -0300

    Added first section into the about page

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def about():
+    return render_template('about.html')
 
-@app.route('/teste')
-def teste():
-    return "TESTE"
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
 
 if __name__ == "__main__":
     app.run()
