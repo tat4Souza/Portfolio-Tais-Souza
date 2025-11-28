2052aca3caf5dcb52709cbce984475bf6a289a79
Tais Souza <tata.ssouz47@gmail.com>
2025-11-28 01:31:51 -0300

    Added certificates slideshow into education page

--- data.py
+++ data.py
@@ -26,3 +26,83 @@ courses_array = [
      "desc": "Abrange módulos focados em ensinar habilidades relacionadas as áreas de design, animação, desenvolvimento de jogos, 3D e edição audiovisual."}
 ]
       
+certificates_array = [
+    {
+        "title": "Advanced CSS and Sass",
+        "place": "Udemy",
+        "image": "images/sass-thumbnail.png",
+        "desc": "Criação de arquitetura de estilos com Sass e técnicas avançadas de CSS (Grid, Flexbox).",
+        "link": "https://www.udemy.com/certificate/UC-74bc68db-eb6d-4598-bbed-911b79768630/"
+    },
+    {
+        "title": "Linux Unhatched",
+        "place": "Cisco Networking Academy",
+        "image": "images/linux-thumbnail.png",
+        "desc": "Introdução ao sistema operacional Linux e comandos básicos de linha para navegação.",
+        "link": "#"
+    },
+    {
+        "title": "Curso Báscio de Git",
+        "place": "Cursa",
+        "image": "images/git-thumbnail.png",
+        "desc": "Aprendizado sobre controle de versão com Git e fluxo de trabalho para projetos de software.",
+        "link": "https://cursa.com.br/certificate/f0010f1b3e"
+    },
+    {
+        "title": "Arte Vetorial",
+        "place": "Escola SAGA",
+        "image": "images/arte-vetorial-thumbnail.png",
+        "desc": "Uso do Illustrator para criação de artes vetoriais, logotipos e ilustrações digitais.",
+        "link": "https://hub.saga.com.br/certificado?chave=E81A2F53"
+    },
+    {
+        "title": "Composição de Imagens",
+        "place": "Escola SAGA",
+        "image": "images/composicao-imagens-thumbnail.png",
+        "desc": "Técnicas de composição e manipulação de imagens digitais usando software Photoshop.",
+        "link": "https://hub.saga.com.br/certificado?chave=31A86CC3"
+    },
+    {
+        "title": "VFX e Motion Graphics",
+        "place": "Escola SAGA",
+        "image": "images/motion-graphics-thumbnail.png",
+        "desc": "Fundamentos de Efeitos Visuais (VFX) e criação de animações e elementos visuais.",
+        "link": "https://hub.saga.com.br/certificado?chave=FED0A422"
+
+    },
+    {
+        "title": "Pintura Digital",
+        "place": "Escola SAGA",
+        "image": "images/pintura-digital-thumbnail.png",
+        "desc": "Domínio das ferramentas e técnicas de pintura digital, iluminação e texturização.",
+        "link": "https://hub.saga.com.br/certificado?chave=43639BB9"
+    },
+    {
+        "title": "3D1 - Cenários",
+        "place": "Escola SAGA",
+        "image": "images/3D-1-thumbnail.png",
+        "desc": "Modelagem 3D de cenários e ambientes, incluindo texturização e renderização.",
+        "link": "https://hub.saga.com.br/certificado?chave=5F6448D2"
+    },
+    {
+        "title": "3D2 - Personagem",
+        "place": "Escola SAGA",
+        "image": "images/3D-2-thumbnail.png",
+        "desc": "Modelagem 3D e escultura digital de personagens, retopologia e mapeamento UV.",
+        "link": "https://hub.saga.com.br/certificado?chave=E294BBE4" 
+    },
+    {
+        "title": "3D3 - Animação",
+        "place": "Escola SAGA",
+        "image": "images/3D-3-thumbnail.png",
+        "desc": "Introdução aos princípios de animação 3D, rigging de personagens e técnicas de movimentação.",
+        "link": "https://hub.saga.com.br/certificado?chave=792C6790"
+    },
+    {
+        "title": "Edição Audiovisual",
+        "place": "Escola SAGA",
+        "image": "images/edicao-audiovisual-thumbnail.png",
+        "desc": "Técnicas de edição e montagem de vídeos, incluindo correção de cor e sincronização de áudio.",
+        "link": "https://hub.saga.com.br/certificado?chave=29CDCC8F"
+    }
+]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from data import data_array, courses_array
+from data import data_array, courses_array, certificates_array
 
 app = Flask(__name__)
 
@@ -13,7 +13,7 @@ def projects():
 
 @app.route('/education')
 def education():
-    return render_template('education.html', data = courses_array)
+    return render_template('education.html', data = courses_array, certificates = certificates_array)
 
 if __name__ == "__main__":
     app.run()
