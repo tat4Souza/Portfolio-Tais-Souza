2036f4d6fa41513b6684e356ab3e89572bf2bd38
Tais Souza <tata.ssouz47@gmail.com>
2025-12-03 20:40:07 -0300

    Removed GSAP animations from mobile screens

--- static/js/about-animation.js
+++ static/js/about-animation.js
@@ -1,75 +1,86 @@
-
-gsap.timeline({ defaults: { duration: .8 } }).from('.header', { opacity: 0, y: '-50' });
-
-const contentTimeline = gsap.timeline({ defaults: { duration: .8 }, paused: true });
 gsap.registerPlugin(ScrollTrigger);
 
-contentTimeline
-    .from('.intro__header', { opacity: 0, x: '-5vw', ease: 'power2.inOut' })
-    .from('.intro__left-container > p', { opacity: 0, ease: 'slow' })
-    .from('.stars--1', { opacity: 0, y: '-300', rotation: 360, ease: 'power4.out' }, 1)
-    .from('.stars--2', { opacity: 0, x: '-15vw', rotation: '360', ease: 'power4.out' }, 1)
-    .from('.stars--3', { opacity: 0, x: '-15vw', rotation: '360', ease: 'power4.out', delay: 0.6 }, 1);
+const contentTimeline = gsap.timeline({ defaults: { duration: .8 }, paused: true });
 
-gsap.to('.intro__right-container img', {
-    y: '80vh',
-    ease: 'none',
 
-    scrollTrigger: {
-        trigger: 'main',
-        start: 'top top',
-        end: '+=1000',
-        scrub: true
-    }
-})
+ScrollTrigger.matchMedia({
 
-const skills = gsap.utils.toArray('.skills-animation');
-skills.forEach(skill_content => {
-    gsap.from(skill_content, {
-        y: -200,
-        opacity: 0,
-        scrollTrigger: {
-            trigger: skill_content,
-            end: '-+20',
-            scrub: true
-        }
-    })
-});
+    "(min-width: 500px)": function () {
 
-gsap.from('.skills-animation--2', {
-    y: 300,
-    opacity: 0,
-    scrollTrigger: {
-        trigger: '.skills',
-        end: '-+30',
-        scrub: true
-    }
-})
 
-ScrollTrigger.create({
-    trigger: '.intro',
-    start: 'top bottom',
+        gsap.timeline({ defaults: { duration: .8 } }).from('.header', { opacity: 0, y: '-50' });
 
-    onEnterBack: () => {
-        contentTimeline.timeScale(1.5).restart(true);
+
+        contentTimeline
+            .from('.intro__header', { opacity: 0, x: '-5vw', ease: 'power2.inOut' })
+            .from('.intro__left-container > p', { opacity: 0, ease: 'slow' })
+            .from('.stars--1', { opacity: 0, y: '-300', rotation: 360, ease: 'power4.out' }, 1)
+            .from('.stars--2', { opacity: 0, x: '-15vw', rotation: '360', ease: 'power4.out' }, 1)
+            .from('.stars--3', { opacity: 0, x: '-15vw', rotation: '360', ease: 'power4.out', delay: 0.6 }, 1);
+
+
+        gsap.to('.intro__right-container img', {
+            y: '80vh',
+            ease: 'none',
+            scrollTrigger: {
+                trigger: 'main',
+                start: 'top top',
+                end: '+=1000',
+                scrub: true
+            }
+        });
+
+
+        const skills = gsap.utils.toArray('.skills-animation');
+        skills.forEach(skill_content => {
+            gsap.from(skill_content, {
+                y: -200,
+                opacity: 0,
+                scrollTrigger: {
+                    trigger: skill_content,
+                    end: '-+20',
+                    scrub: true
+                }
+            });
+        });
+
+
+        gsap.from('.skills-animation--2', {
+            y: 300,
+            opacity: 0,
+            scrollTrigger: {
+                trigger: '.skills',
+                end: '-+30',
+                scrub: true
+            }
+        });
+
+
+        ScrollTrigger.create({
+            trigger: '.intro',
+            start: 'top bottom',
+
+            onEnterBack: () => {
+                contentTimeline.timeScale(1.5).restart(true);
+            },
+
+            onEnter: () => {
+                contentTimeline.timeScale(1).restart(true);
+            }
+        });
+
+
+        const competencies = gsap.utils.toArray('.competencies-animation');
+        gsap.from(competencies, {
+            x: -200,
+            opacity: 0,
+            stagger: 0.3,
+            scrollTrigger: {
+                trigger: competencies,
+                start: 'top 80%',
+                end: '+=280',
+                scrub: 1,
+            }
+        });
     },
-
-    onEnter: () => {
-        contentTimeline.timeScale(1).restart(true);
-    }
-});
-
-
-const competencies = gsap.utils.toArray('.competencies-animation');
-gsap.from(competencies, {
-    x: -200,
-    opacity: 0,
-    stagger: 0.3,
-
-    scrollTrigger: {
-        trigger: competencies,
-        start: 'top 80%',
-        end: '+=280',
-        scrub: 1,
-    }
 });
--- static/js/education-animation.js
+++ static/js/education-animation.js
@@ -10,67 +10,72 @@ document.querySelectorAll('.education__course').forEach(card => {
     });
 });
 
+
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from('.education h2', { duration: 1, y: -10 });
-gsap.from('.certificates h2', { duration: 1, x: -200, opacity: 0, scrollTrigger: { trigger: 'certificates', scrub: true } });
 
-gsap.fromTo('.education h2',
-    { x: 0, opacity: 1 },
+ScrollTrigger.matchMedia({
 
-    {
-        x: '200',
-        opacity: 0,
-        ease: 'none',
-
-        scrollTrigger: {
-            trigger: '.education',
-            start: 'top top',
-            end: '+=300',
-            scrub: true,
-        }
-    }
-);
+    "(min-width: 500px)": function () {
+        gsap.from('.education h2', { duration: 1, y: -10 });
 
 
-// gsap.to('.education__course', {
-//     duration: 0.5,
-//     opacity: 1,
-//     left: 0,
-//     stagger: 0.1,
-//     ease: 'power2.out',
-// });
-
-gsap.fromTo('.education__course',
-    // ESTADO INICIAL (FROM)
-    { opacity: 0, left: -100 },
-
-    // ESTADO FINAL (TO)
-    {
-        duration: 0.5,
-        opacity: 1,
-        left: 0,
-        stagger: 0.1,
-        ease: 'power2.out'
-    }
-);
+        gsap.from('.certificates h2', {
+            duration: 1,
+            x: -200,
+            opacity: 0,
+            scrollTrigger: {
+                trigger: 'certificates',
+                scrub: true
+            }
+        });
 
 
-const boxes = gsap.utils.toArray('.education__course');
-boxes.forEach(box => {
-    gsap.set(box, { opacity: 1, left: 0 });
+        gsap.fromTo('.education h2',
+            { x: 0, opacity: 1 },
+            {
+                x: '200',
+                opacity: 0,
+                ease: 'none',
+                scrollTrigger: {
+                    trigger: '.education',
+                    start: 'top top',
+                    end: '+=300',
+                    scrub: true,
+                }
+            }
+        );
 
-    gsap.to(box, {
-        left: -50,
-        opacity: 0,
-        ease: 'power2.out',
 
-        scrollTrigger: {
-            trigger: box,
-            start: 'bottom 50%',
-            end: 'bottom',
-            scrub: true,
-        }
-    });
+        gsap.fromTo('.education__course',
+            { opacity: 0, left: -100 },
+            {
+                duration: 0.5,
+                opacity: 1,
+                left: 0,
+                stagger: 0.1,
+                ease: 'power2.out'
+            }
+        );
+
+
+        const boxes = gsap.utils.toArray('.education__course');
+        boxes.forEach(box => {
+            gsap.set(box, { opacity: 1, left: 0 });
+
+            gsap.to(box, {
+                left: -50,
+                opacity: 0,
+                ease: 'power2.out',
+                scrollTrigger: {
+                    trigger: box,
+                    start: 'bottom 50%',
+                    end: 'bottom',
+                    scrub: true,
+                }
+            });
+        });
+
+
+    },
 });
-
